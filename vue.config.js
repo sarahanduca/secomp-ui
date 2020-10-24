4d5cdbf16044a02133eba852a4132dca6b7f63c6
bfsgr <bfsgr.code@protonmail.com>
2020-10-24 19:02:33 -0300

    support resolving images in b-img

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,27 @@
 module.exports = {
   configureWebpack: {
     devtool: "source-map"
+  },
+  chainWebpack: config => {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap(options => {
+        options.transformAssetUrls = {
+          img: "src",
+          image: "xlink:href",
+          "b-avatar": "src",
+          "b-img": "src",
+          "b-img-lazy": ["src", "blank-src"],
+          "b-card": "img-src",
+          "b-card-img": "src",
+          "b-card-img-lazy": ["src", "blank-src"],
+          "b-carousel-slide": "img-src",
+          "b-embed": "src"
+        };
+
+        return options;
+      });
   }
 };

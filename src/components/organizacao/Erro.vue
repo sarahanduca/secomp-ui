d579b9f6123e1e921afea49f2602121fbed2fc31
bfsgr <bfsgr.code@protonmail.com>
2020-11-01 00:20:29 -0300

    página de erro nova

--- src/components/organizacao/Erro.vue
+++ src/components/organizacao/Erro.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <p>Houve um erro</p>
+  <div class="w-100 h-100">
+    <b-row align-h="center" align-v="center" class="mx-0 h-100">
+      <b-col class="text-center">
+        <b-img src="../../assets/secompBranca.svg" width="150" />
+        <h2 class="mt-2">Algo deu errado!</h2>
+        <b-button class="mt-2" variant="light" to="/"
+          >Voltar para o início</b-button
+        >
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -10,4 +18,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+h2 {
+  color: #296016;
+}
+</style>
